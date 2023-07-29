497a559642a6d462507d23dd084e536396f59ac1
tadghh <thenry.he@gmail.com>
2023-07-28 22:54:55 -0500

    File input styling fix

--- components/settings/setting-item-components.js
+++ components/settings/setting-item-components.js
@@ -3,8 +3,8 @@ import ToggleButton from "../Inputs/toggle-button";
 import { useEffect } from "react";
 const FileSettingComponent = ({ setter, status = "" }) => {
   return (
-    <span
-      className="flex-none rounded-md bg-white border-indigo-600 border-2 px-5 py-2.5 text-sm font-semibold text-blue-500 shadow-sm"
+    <div
+      className="whitespace-pre border-2 border-indigo-600 location-input px-5 py-2.5 rounded-md "
       onClick={() => {
         open({
           directory: true,
@@ -16,8 +16,10 @@ const FileSettingComponent = ({ setter, status = "" }) => {
         });
       }}
     >
-      {status}
-    </span>
+      <span className="flex-none font-semibold text-blue-500 shadow-sm">
+        {status}
+      </span>
+    </div>
   );
 };
 const ToggleSettingComponent = ({ setter, status }) => {
