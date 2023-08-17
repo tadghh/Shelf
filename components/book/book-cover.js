e04b1ff8862162bc333db59baad6bf51dfc50e95
tadghh <thenry.he@gmail.com>
2023-08-16 22:53:34 -0500

    Next page button styling

--- components/book/book-cover.js
+++ components/book/book-cover.js
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 
-
-
 export default function BookCover({ title, coverPath }) {
 	const bookLink = `./books/${title}`;
 	return (
-		<Link  href={bookLink} className=" duration-500 h-[500px]
+		<Link href={bookLink} className=" duration-500 h-[500px]
 		 w-[300px] border-2 hover:border-white border-black rounded-lg bg-white px-3 pb-8 pt-5
-		  text-black  transition ease-in-out hover:bg-black hover:text-white ">
+		  text-black  transition ease-in-out hover:bg-black hover:text-white  overflow-clip">
 			<div className="flex flex-col justify-between h-full ">
 				<div className="flex justify-center overflow-hidden grow max-w-fit h-4/5 max-h-fit ">
 					<Image
@@ -22,7 +20,7 @@ export default function BookCover({ title, coverPath }) {
 					/>
 				</div>
 
-				<div className="self-start max-w-xs pt-2 text-base font-semibold h-1/5 ">
+				<div className="self-start max-w-xs pt-2 text-base font-semibold h-1/5">
 					<span >{title}</span>
 				</div>
 			</div>
--- pages/books/[book]/index.js
+++ pages/books/[book]/index.js
@@ -5,130 +5,153 @@ import { useState, useEffect, useRef } from "react";
 import ePub from "epubjs";
 
 export default function Book() {
+  const router = useRouter();
+  const { book } = router.query;
+  const bookRef = useRef(null);
+  const [bookData, setBookData] = useState("");
+  const [bookOpen, setBookOpen] = useState(false);
+  const [bookRender, setBookRender] = useState();
+  const [scrollStyle, setScrollStyle] = useState("false");
 
-	const router = useRouter();
-	const { book } = router.query;
-	const bookRef = useRef(null);
-	const [bookData, setBookData] = useState("");
-	const [bookOpen, setBookOpen] = useState(false);
-	const [bookRender, setBookRender] = useState();
-	const [scrollStyle, setScrollStyle] = useState("false");
+  const [backgroundData, setBackgroundData] = useState(null);
+  const [bookLoaded, setBookLoaded] = useState(false);
 
-	const [backgroundData, setBackgroundData] = useState(null);
-	const [bookLoaded, setBookLoaded] = useState(false);
+  const [viewerHeight, setViewerHeight] = useState(window.innerHeight - 40); // Adjust margin as needed
+  const [viewerWidth, setViewerWidth] = useState(window.innerWidth - 40); // Adjust margin as needed
+  useEffect(() => {
+    const handleResize = () => {
+      setViewerHeight(window.innerHeight - 40); // Adjust margin as needed
+      setViewerWidth(window.innerWidth - 140); // Adjust margin as needed
+    };
 
-	useEffect(() => {
-		async function loadBook() {
-			if (book !== undefined && !bookOpen) {
+    window.addEventListener("resize", handleResize);
 
-				setBookData(await invoke("load_book", { title: book }));
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  useEffect(() => {
+    async function loadBook() {
+      if (book !== undefined && !bookOpen) {
+        setBookData(await invoke("load_book", { title: book }));
 
-				const bookLoaded = ePub({
-					encoding: "base64",
-				});
+        const bookLoaded = ePub({
+          encoding: "base64",
+        });
 
-				if (bookData.length !== 0 && !bookLoaded.isOpen) {
-					let endlessScrollValue;
+        if (bookData.length !== 0 && !bookLoaded.isOpen) {
+          let endlessScrollValue;
 
-					invoke("get_cover", { book_title: book }).then((data) => {
-						setBackgroundData(data);
-					});
+          invoke("get_cover", { book_title: book }).then((data) => {
+            setBackgroundData(data);
+          });
 
-					invoke("get_configuration_option", {
-						option_name: "endless_scroll",
-					}).then(data => {
-						if (data) {
-							endlessScrollValue = data;
+          invoke("get_configuration_option", {
+            option_name: "endless_scroll",
+          }).then((data) => {
+            if (data) {
+              endlessScrollValue = data;
 
-							setScrollStyle(endlessScrollValue === "true");
-						}
-					});
+              setScrollStyle(endlessScrollValue === "true");
+            }
+          });
 
-					bookRef.current = bookLoaded;
+          bookRef.current = bookLoaded;
 
-					bookLoaded.open(bookData);
+          bookLoaded.open(bookData);
 
-					try {
-						bookLoaded.ready.then(() => {
-							//I dont like this null here but nmp atm
+          try {
+            bookLoaded.ready.then(() => {
+              //I dont like this null here but nmp atm
 
-							const rendition = bookLoaded.renderTo(
-								document.getElementById("viewer"),
-								{
-									manager: endlessScrollValue === "true" ? "continuous" : "default",
-									flow: endlessScrollValue === "true" ? "scrolled" : null,
-									width: "100%",
-									height: "100%",
-								}
-							);
+              const rendition = bookLoaded.renderTo(
+                document.getElementById("viewer"),
+                {
+                  manager:
+                    endlessScrollValue === "true" ? "continuous" : "default",
+                  flow: endlessScrollValue === "true" ? "scrolled" : null,
+                  width: "100%",
+                  height: "80%",
+                }
+              );
 
-							setBookRender(rendition);
-							rendition.display();
-						});
-					} catch {
-						//handle this
-					}
-					setBookLoaded(true);
-					setBookOpen(true);
-				}
-			}
-		}
+              setBookRender(rendition);
+              rendition.display();
+            });
+          } catch {
+            //handle this
+          }
+          setBookLoaded(true);
+          setBookOpen(true);
+        }
+      }
+    }
 
-		loadBook();
-	}, [book, bookRef, bookData, bookOpen]);
+    loadBook();
+  }, [book, bookRef, bookData, bookOpen]);
 
-	useEffect(() => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      import("@tauri-apps/api/window").then((module) => {
+        const { appWindow } = module;
+        appWindow.setTitle(book);
+      });
+    }
 
-		if (typeof window !== "undefined") {
-			import("@tauri-apps/api/window").then((module) => {
-				const { appWindow } = module;
-				appWindow.setTitle(book);
-			});
-		}
+    router.events.on("routeChangeStart", () => {
+      if (typeof window !== "undefined") {
+        import("@tauri-apps/api/window").then((module) => {
+          const { appWindow } = module;
+          appWindow.setTitle("Shelf");
+        });
+      }
+    });
+  }, [book, router.events]);
+  //You can break it by squishing the window to small than it cant scroll
 
-		router.events.on('routeChangeStart', () => {
-			if (typeof window !== "undefined") {
-				import("@tauri-apps/api/window").then((module) => {
-					const { appWindow } = module;
-					appWindow.setTitle("Shelf");
-				});
-			}
-		});
-
-
-	}, [book, router.events]);
-	//You can break it by squishing the window to small than it cant scroll
-	return (
-		<>
-			{bookLoaded &&
-				<div className="flex flex-col items-center max-h-screen justify-items-center " style={{ backgroundImage: `url('data:image/jpeg;base64,${backgroundData}')` }}>
-					<div className="flex flex-col items-center w-full h-full overflow-hidden backdrop-filter backdrop-blur justify-items-center " >
-						<div id="viewer" className="bg-white overflow-hidden ml-20 border-2 border-orange-700 my-10 rounded-lg w-[800px] h-[1800px]"  >
-
-							{scrollStyle ? null :
-								<div id="controls" className="flex justify-between">
-									<button
-										onClick={() => bookRender.prev()}
-										type="button"
-										className="px-2 py-1 text-xs font-semibold text-gray-900 bg-white rounded shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-									>
-										Previous
-									</button>
-									<button
-										onClick={() => bookRender.next()}
-										type="button"
-										className="px-2 py-1 text-xs font-semibold text-gray-900 bg-white rounded shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-									>
-										Next
-									</button>
-								</div>
-							}
-
-						</div>
-					</div>
-
-				</div>
-			}
-		</>
-	);
+  //div overlaps the book fade to edge with black
+  return (
+    <>
+      {bookLoaded && (
+        <div
+          className="flex flex-col items-center max-h-screen justify-items-center "
+          //   style={{
+          //     backgroundImage: `url('data:image/jpeg;base64,${backgroundData}')`,
+          //   }}
+        >
+          <div className="flex flex-col items-center my-5 backdrop-filter backdrop-blur justify-items-center ">
+            {scrollStyle ? (
+              <div
+                id="viewer"
+                className="bg-white overflow-hidden ml-20  my-10 rounded w-[800px] h-auto "
+              ></div>
+            ) : (
+              <div
+                id="controls"
+                className="z-50 flex justify-between ml-20 border rounded-xl max-w-[800px]  "
+                style={{
+                  height: `${viewerHeight}px`,
+                  width: `${viewerWidth}px`,
+                }}
+              >
+                <div
+                  onClick={() => bookRender.prev()}
+                  className="w-[20px] h-auto px-2 text-xs font-semibold text-gray-900 shadow-sm grow-0 "
+                ></div>
+                <div
+                  id="viewer"
+                  className="bg-white "
+                  style={{ width: `${viewerWidth}px` }}
+                ></div>
+                <div
+                  onClick={() => bookRender.next()}
+                  className="px-2 py-1 w-[20px] text-xs font-semibold text-gray-900 shadow-sm grow-0 "
+                ></div>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
