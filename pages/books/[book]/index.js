0e1588089b4721d0717bceac7f00553ef27ed714
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-08-30 21:59:36 -0500

    Refactor book loading and epub setup
    Different approach for loading settings for epubjs, changed the useEffect to make sure the page is ready before calling the loadBook method

--- pages/books/[book]/index.js
+++ pages/books/[book]/index.js
@@ -7,37 +7,37 @@ import ePub from "epubjs";
 
 export default function Book() {
   const router = useRouter();
+  const bookRef = useRef();
+
   const { book } = router.query;
-  const bookRef = useRef(null);
-  const [bookData, setBookData] = useState("");
+
+  const [bookData, setBookData] = useState();
   const [bookOpen, setBookOpen] = useState(false);
+  const [bookLoaded, setBookLoaded] = useState(false);
   const [bookRender, setBookRender] = useState();
+
   const [scrollStyle, setScrollStyle] = useState(false);
 
-  const [bookLoaded, setBookLoaded] = useState(false);
-
-  //calculate the width of the book
+  //calculate the width of the book without margins
   const bookSize = () => {
     return window.innerWidth - 160 > 800 ? 800 : window.innerWidth - 180;
   };
-  console.log("Width" + (window.innerWidth - 20)  );
-  console.log("Widtdh" + bookSize(0) );
+
   const [viewerHeight, setViewerHeight] = useState(window.innerHeight - 40);
-  const [viewerWidth, setViewerWidth] = useState(bookSize(0));
+  const [viewerWidth, setViewerWidth] = useState(bookSize());
+
   useEffect(() => {
     invoke("get_configuration_option", {
       option_name: "endless_scroll",
     }).then((data) => {
       if (data) {
-
-
         setScrollStyle(data === "true");
       }
     });
   });
+
   useEffect(() => {
     const handleResize = () => {
-      //less than the max and greater than the min
       setViewerHeight(window.innerHeight - 40);
       setViewerWidth(window.innerWidth - 140);
       if (bookRender) {
@@ -51,54 +51,53 @@ export default function Book() {
       window.removeEventListener("resize", handleResize);
     };
   }, [bookRender]);
+
   useEffect(() => {
     async function loadBook() {
-      if (book !== undefined && !bookOpen) {
-        setBookData(await invoke("load_book", { title: book }));
+      setBookData(await invoke("load_book", { title: book }));
 
-        const bookLoaded = ePub({
-          encoding: "base64",
-        });
+      const bookLoaded = ePub({
+        encoding: "base64",
+      });
 
-        if (bookData.length !== 0 && !bookLoaded.isOpen) {
+      if (bookData && !bookLoaded.isOpen) {
+        bookRef.current = bookLoaded;
 
+        bookLoaded.open(bookData);
 
+        try {
+          bookLoaded.ready.then(() => {
+            let bookWidth = bookSize() + "";
+            let settings = {
+              width: bookWidth,
+              height: window.innerHeight - 40,
+              spread: "none",
+            };
 
-          bookRef.current = bookLoaded;
-
-          bookLoaded.open(bookData);
-
-          try {
-            bookLoaded.ready.then(() => {
-              //I dont like this null here but nmp atm
-              let bookWidth = bookSize() + "";
-
-              const rendition = bookLoaded.renderTo(
-                document.getElementById("viewer"),
-                {
-                  manager:
-                  scrollStyle  ? "continuous" : "default",
-                  flow: scrollStyle ? "scrolled" : null,
-                  width: bookWidth,
-                  height: window.innerHeight - 40,
-                  spread: "none",
+            scrollStyle
+              ? () => {
+                  settings.manager = "continuous";
+                  settings.flow = "scrolled";
                 }
-              );
-              // rendition.resize(800, window.innerHeight - 40 );
-              console.log(rendition);
-              setBookRender(rendition);
-              rendition.display();
-            });
-          } catch {
-            //handle this
-          }
-          setBookLoaded(true);
-          setBookOpen(true);
+              : (settings.manager = "default");
+
+            const rendition = bookLoaded.renderTo(
+              document.getElementById("viewer"),
+              settings
+            );
+            setBookRender(rendition);
+            rendition.display();
+          });
+        } catch {
+          //handle this
         }
+        setBookLoaded(true);
+        setBookOpen(true);
       }
     }
-
-    loadBook();
+    if (book !== undefined && !bookOpen) {
+      loadBook();
+    }
   }, [book, bookRef, bookData, bookOpen]);
 
   useEffect(() => {
@@ -110,41 +109,33 @@ export default function Book() {
       appWindow.setTitle("Shelf");
     });
   }, [book, router.events]);
-  //You can break it by squishing the window to small than it cant scroll
 
-  //div overlaps the book fade to edge with black
   return (
     <>
       {bookLoaded && (
-        <div
-          className="flex flex-col items-center max-h-screen justify-items-center "
-
-        >
-          <div className="flex flex-col items-center my-5 ml-20 backdrop-filter backdrop-blur justify-items-center " style={{
-                  height: `${viewerHeight}px`,
-                  width: `${viewerWidth}px`,
-                }}>
+        <div className="flex flex-col items-center max-h-screen justify-items-center">
+          <div
+            className="flex flex-col items-center my-5 ml-20 backdrop-filter backdrop-blur justify-items-center "
+            style={{
+              height: `${viewerHeight}px`,
+              width: `${viewerWidth}px`,
+            }}
+          >
             {scrollStyle ? (
               <div
                 id="viewer"
                 className="bg-white rounded-xl overflow-clip  max-w-[800px]  "
-
               />
             ) : (
               <div
                 id="controls"
                 className="z-40 flex justify-between border rounded-xl max-w-[840px]  overflow-hidden"
-
               >
                 <div
                   onClick={() => bookRender.prev()}
                   className="w-[20px] h-auto px-2 text-xs font-semibold bg-gradient-to-r from-black to-white text-gray-900 shadow-sm grow-0 "
                 ></div>
-                <div
-                  id="viewer"
-                  className="bg-white "
-
-                />
+                <div id="viewer" className="bg-white " />
                 <div
                   onClick={() => bookRender.next()}
                   className="px-2 py-1 w-[20px] z-50 text-xs font-semibold text-gray-900 bg-gradient-to-l from-black to-white shadow-sm grow-0 "
