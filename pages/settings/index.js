3a46138c3cdc681fa32cd535e7d06e2e92c4856f
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-10-02 05:26:40 -0500

    fixed reset settings

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -14,7 +14,9 @@ export default function Settings() {
       setSettingsItemsEnum(data);
     });
   }, []);
-
+  const resetHandler = () => {
+    invoke("reset_configuration");
+  };
   return (
     <div className="duration-550 ml-20 min-h-screen animate-fade flex-col px-5 py-2 transition-opacity ease-in-out">
       {settingsItemsEnum.length != 0 ? (
@@ -41,6 +43,7 @@ export default function Settings() {
             <button
               className=" rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
               type="button"
+              onClick={resetHandler}
             >
               Reset settings
             </button>
