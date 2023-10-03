f63ede92dab783e7900e7e0a978927ea556b59ad
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-10-03 00:36:37 -0500

    cleaning

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 
 const nextConfig = {
 	reactStrictMode: true,
-	// Note: This feature is required to use NextJS Image in SSG mode.
-	// See https://nextjs.org/docs/messages/export-image-api for different workarounds.
 	images: {
 		unoptimized: true,
 	},
