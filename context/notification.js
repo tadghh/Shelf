f7da72284e370e198c121b0f3f93504c755695ed
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-10-04 00:29:51 -0500

    add framer

---
+++ context/notification.js
@@ -0,0 +1,122 @@
+import React, {
+  createContext,
+  useCallback,
+  useEffect,
+  useReducer,
+} from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+import NotificationCard from "../Components/NotificationCard";
+
+import {
+  notificationState,
+  notificationInitialState,
+  notificationReducer,
+} from "./lib/notifications";
+
+export const NotificationContext = createContext();
+
+function Notification({ children }) {
+  const [state, dispatch] = useReducer(
+    notificationReducer,
+    notificationInitialState,
+  );
+
+  const deleteNotifcation = (id) => {
+    dispatch({
+      type: notificationState.DELETE,
+      payload: {
+        id: id,
+      },
+    });
+  };
+
+  const closeNotification = (id) => {
+    dispatch({
+      type: notificationState.INACTIVE,
+      payload: {
+        id: id,
+      },
+    });
+    setTimeout(() => {
+      deleteNotifcation(id);
+    }, 1000);
+  };
+
+  const notify = (type, message) => {
+    const notificationId = state.notifications.length;
+    dispatch({
+      type: notificationState.ADD,
+      payload: {
+        notification: {
+          id: notificationId,
+          type: type,
+          message: message,
+          active: true,
+        },
+      },
+    });
+    setTimeout(() => {
+      closeNotification(notificationId);
+    }, 6000);
+    return notificationId;
+  };
+
+  const showNotifications = useCallback(
+    () => (
+      <>
+        {state.notifications.map((notification) => (
+          <AnimatePresence key={notification?.id}>
+            {notification?.active && (
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  scale: 0.8,
+                  y: "10%",
+                }}
+                animate={{
+                  opacity: 1,
+                  scale: 1,
+                  y: "0%",
+                }}
+                exit={{
+                  opacity: 0,
+                  scale: 0.8,
+                  y: "10%",
+                }}
+              >
+                <NotificationCard
+                  type={notification?.type}
+                  message={notification?.message}
+                />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        ))}
+      </>
+    ),
+    [state],
+  );
+
+  useEffect(() => {
+    state;
+  }, [state]);
+
+  const value = {
+    notifications: state?.notifications,
+    notify,
+    closeNotification,
+  };
+  return (
+    <>
+      <NotificationContext.Provider value={value}>
+        <div className="fixed left-0 top-0 z-50 flex h-fit w-full flex-col items-center justify-center gap-3 pt-10">
+          {showNotifications()}
+        </div>
+        {children}
+      </NotificationContext.Provider>
+    </>
+  );
+}
+
+export default Notification;
