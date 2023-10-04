6e225fd9537cc4dec1b7722bb4861eed3c14426c
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-10-03 22:30:38 -0500

    styling

--- components/Inputs/toggle-button.js
+++ components/Inputs/toggle-button.js
@@ -12,14 +12,14 @@ export default function ToggleButton({ state }) {
       checked={enabled}
       onChange={setEnabled}
       className={classNames(
-        enabled ? "bg-indigo-600" : "bg-gray-200",
-        "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none ring-2 ring-indigo-600 "
+        enabled ? "bg-indigo-600 ring-green-600" : "bg-gray-200 ring-red-700",
+        "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent ring-2  transition-colors duration-200 ease-in-out focus:outline-none ",
       )}
     >
       <span
         className={classNames(
           enabled ? "translate-x-5" : "translate-x-0",
-          "pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+          "pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out",
         )}
       >
         <span
@@ -27,12 +27,12 @@ export default function ToggleButton({ state }) {
             enabled
               ? "opacity-0 duration-100 ease-out"
               : "opacity-100 duration-200 ease-in",
-            "absolute inset-0 flex h-full w-full items-center justify-center transition-opacity"
+            "absolute inset-0 flex h-full w-full items-center justify-center transition-opacity",
           )}
           aria-hidden="true"
         >
           <svg
-            className="w-3 h-3 text-gray-400"
+            className="h-3 w-3 text-gray-400"
             fill="none"
             viewBox="0 0 12 12"
           >
@@ -50,12 +50,12 @@ export default function ToggleButton({ state }) {
             enabled
               ? "opacity-100 duration-200 ease-in"
               : "opacity-0 duration-100 ease-out",
-            "absolute inset-0 flex h-full w-full items-center justify-center transition-opacity"
+            "absolute inset-0 flex h-full w-full items-center justify-center transition-opacity",
           )}
           aria-hidden="true"
         >
           <svg
-            className="w-3 h-3 text-indigo-600"
+            className="h-3 w-3 text-indigo-600"
             fill="currentColor"
             viewBox="0 0 12 12"
           >
